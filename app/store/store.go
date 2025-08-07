08fab49c20948b46df4ae8400a62cfa5b2b3eed0
tang-jiapeng <3081129260@qq.com>
2025-08-07 12:44:27 +0800

    delete useless code

--- app/store/store.go
+++ app/store/store.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
@@ -36,32 +35,4 @@ type ListOps interface {
 	GetListRange(key string, start, stop int) ([]string, error)
 	GetListLength(key string) (int, error)
 	PopLElement(key string) (string, bool, error)
-}
-
-// GetData 获取键对应的数据
-func (s *Store) GetData(key string) (DataType, bool) {
-	s.RLock()
-	defer s.RUnlock()
-	data, exists := s.m[key]
-	if !exists {
-		fmt.Printf("Store: GetData key=%s, exists=false\n", key)
-		return nil, false
-	}
-	return data, true
-}
-
-// SetData 设置键值对
-func (s *Store) SetData(key string, data DataType) {
-	s.Lock()
-	defer s.Unlock()
-	s.m[key] = data
-	fmt.Printf("Store: SetData key=%s, type=%s\n", key, data.Type())
-}
-
-// Delete 删除键
-func (s *Store) Delete(key string) {
-	s.Lock()
-	defer s.Unlock()
-	delete(s.m, key)
-	fmt.Printf("Store: Delete key=%s\n", key)
 }
