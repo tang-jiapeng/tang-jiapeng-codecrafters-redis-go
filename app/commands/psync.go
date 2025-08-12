0397f241fa9a33efad1fdeb5d63f08d8433285a9
tang-jiapeng <3081129260@qq.com>
2025-08-12 16:29:07 +0800

    fix : response

--- app/commands/psync.go
+++ app/commands/psync.go
@@ -6,6 +6,6 @@ type PsyncCommand struct {
 }
 
 func (c *PsyncCommand) Handle(ctx *ConnectionContext, args []string) (string, error) {
-	response := "FULLRESYNC" + MasterReplID + " 0"
+	response := "FULLRESYNC " + MasterReplID + " 0"
 	return resp.EncodeSimpleString(response), nil
 }
